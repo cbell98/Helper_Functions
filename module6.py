94e7a7a0c0d0d9e6ccbe72df5d5cc35d68c747fd
cbell98 <93108744+cbell98@users.noreply.github.com>
2022-03-14 17:29:53 -0400

    Update module6.py

--- module6.py
+++ module6.py
@@ -33,4 +33,15 @@ def solution(n):
         
     return int(new_str)
 
+# This function accepts an array. It should return true if the array elements read 
+# the same forward and backward, i.e. if the array is a palindrome. An empty array is a palindrome.
+def solution(a):
+    
+    new_arr = []
+    
+    for i in range(len(a)-1, -1, -1):
+        new_arr.append(a[i])
+    
+    return new_arr == a
 
+# 
