b80d90237cd90150b5411f0f3289bd6a33549f78
cbell98 <93108744+cbell98@users.noreply.github.com>
2022-03-15 14:06:07 -0400

    Update module7.py

--- module7.py
+++ module7.py
@@ -128,3 +128,11 @@ def solution(a, x):
 
 # You have an array p of points on a Cartesian plane. Find and return the minimum possible Euclidian distance 
 # between two points with different indices in p. 
+Need answer
+
+# An odometer palindrome is the when the mileage on your odometer is the same when the digits are reversed. 
+# For example, 123321. Assume your odometer is 6 digits and does not show 10ths of miles/kilometers.
+# The question is, for a given current odometer reading, when is the next odometer palindrome?
+# If the odometer is currently at a palindrome, we're still interested in the next one.
+# The input is an integer, but the result should be a string padding with leading zeros to 6 places.
+Need answer
