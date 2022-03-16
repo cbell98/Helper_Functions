3b6d0a9e254e6d94fa77024950e05799a0df0c9c
cbell98 <93108744+cbell98@users.noreply.github.com>
2022-03-16 10:24:00 -0400

    Create moduel8.py

---
+++ moduel8.py
@@ -0,0 +1,25 @@
+# Moving windows
+
+
+# Write a function that processes an input array, returning an array with the difference between each two subsequent elements.
+# For example, if the first two elements in the input are 10 and 6 and 15, then the first two elements in
+# the output will be 4 and -9. (Because 10-6=4 and 6-15=-9.)
+# Note that the output array will have one fewer elements than the input.
+def solution(a):
+    result = []
+    
+    window = 2
+    
+    for start in range(0, len(a) - window + 1):
+        
+        for i in range(0 + start, window + start):
+            diff = (a[0 + start] - a[window + start - 1])
+        
+        result.append(diff)
+        
+    return result
+
+
+# Write a function that moves a "window" over an array, processing the values under the window as it goes.
+# For example, a window of size 3 moving across an array of length 9 might look like this for the first 3 
+# moves (window is the bar over the values):
