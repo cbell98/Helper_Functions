79d765102da2856ef94acd8a5d2a8b9a45ee1696
Colin Bell <93108744+cbell98@users.noreply.github.com>
2022-03-22 11:58:36 -0400

    Update module10.py

--- module10.py
+++ module10.py
@@ -24,4 +24,32 @@ def solution(nums):
     return result
 
 
+# Produce a histogram of word frequency in a string. The function should return an array of strings, where each string takes the form:
+# word: ####
+# word is the word in question, followed by a colon and a space, followed by a number of hash marks that corresponds to the number of 
+# times that word appears in the input string. The result array should be sorted alphabetically. This can be done by calling .sort() 
+# of the array before returning it. If the input string is empty, return an empty array.
+def solution(s):
+
+# Separate string s into list of words s (do i need to do this?)
+    s = s.split()
+        
+    cache = {}
+
+    for word in s:
+        if word not in cache:
+            cache[word] = ""
+        
+        cache[word] += "#"
+    
+    result = []
+    
+    for word in cache:
+        result.append(f"{word}: {cache[word]}")
+    
+    result.sort()
+    
+    return result
+
+
 # 
