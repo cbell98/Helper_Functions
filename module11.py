4ec71876dbfb3916e6e018b15ac2c68a2cc23d30
Colin Bell <93108744+cbell98@users.noreply.github.com>
2022-03-22 16:05:55 -0400

    Create module11.py

---
+++ module11.py
@@ -0,0 +1,28 @@
+# Given three values, return a new linked list containing those values. You have to wrap the values into ListNode() objects.
+# JavaScript:
+# const new_node = new ListNode(n);
+# Python:
+# new_node = ListNode(n)
+# You do not need to uncomment the ListNode code in the header. That's just there for your reference.
+# Note the the tests will show the linked lists as if they are arrays. This is just the visual representation; 
+# under the hood, it's a linked list with those values. Return a reference to the head of the new list.
+
+# Singly-linked lists are already defined with this interface:
+# class ListNode(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.next = None
+
+def solution(a, b, c):
+    
+    a_node = ListNode(a)
+    b_node = ListNode(b)
+    c_node = ListNode(c)
+    
+    a_node.next = b_node
+    b_node.next = c_node
+    
+    return a_node
+
+
+# Above function generalize
