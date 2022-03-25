82a723dae17c0f69fbeb26cd1c7ab7fb57e4e3dc
Colin Bell <93108744+cbell98@users.noreply.github.com>
2022-03-25 01:13:00 -0400

    Update codesignal_arcade.py

--- codesignal_arcade.py
+++ codesignal_arcade.py
@@ -55,4 +55,15 @@ def solution(n):
 # Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any two 
 # neighboring numbers is equal (note that 0 and n - 1 are neighboring, too). Given n and firstNumber, 
 # find the number which is written in the radially opposite position to firstNumber.
+def solution(n, firstNumber):
+    if firstNumber < (0.5 * n):
+        return (n/2 + firstNumber)
+    else:
+        return (firstNumber - (n/2))
 
+
+# One night you go for a ride on your motorcycle. At 00:00 you start your engine, and the built-in timer automatically begins 
+# counting the length of your ride, in minutes. Off you go to explore the neighborhood. When you finally decide to head back, 
+# you realize there's a chance the bridges on your route home are up, leaving you stranded! Unfortunately, you don't have your 
+# watch on you and don't know what time it is. All you know thanks to the bike's timer is that n minutes have passed since 00:00.
+# Using the bike's timer, calculate the current time. Return an answer as the sum of digits that the digital timer in the format hh:mm would show.
