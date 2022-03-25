06380836bebaa8126087e0f52a206dda88957b58
Colin Bell <93108744+cbell98@users.noreply.github.com>
2022-03-25 12:07:09 -0400

    Update codesignal_arcade.py

--- codesignal_arcade.py
+++ codesignal_arcade.py
@@ -137,3 +137,28 @@ def solution(value1, weight1, value2, weight2, maxW):
 
 # You're given three integers, a, b and c. It is guaranteed that two of these integers are equal to each other. 
 # What is the value of the third integer?
+def solution(a, b, c):
+    if a == b:
+        return c
+    if a == c:
+        return b
+    if b == c:
+        return a
+
+
+# Given integers a and b, determine whether the following pseudocode results in an infinite loop
+# while a is not equal to b do
+#   increase a by 1
+#   decrease b by 1
+# Assume that the program is executed on a virtual machine which can store arbitrary long numbers and execute forever.
+def solution(a, b):
+    if a == b:
+        return False
+    if a < (b - 1) and (b - a - 1) % 2 == 1:
+        return False
+    else:
+        return True
+
+
+# Consider an arithmetic expression of the form a#b=c. Check whether it is possible to replace # with one of the
+# four signs: +, -, * or / to obtain a correct expression.
