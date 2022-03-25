80cbe26d4eb5058dff746ee42a6427051a66471a
Colin Bell <93108744+cbell98@users.noreply.github.com>
2022-03-25 11:26:21 -0400

    Update codesignal_arcade.py

--- codesignal_arcade.py
+++ codesignal_arcade.py
@@ -118,4 +118,7 @@ def solution(experience, threshold, reward):
     return experience + reward >= threshold
 
 
-# 
+# You found two items in a treasure chest! The first item weighs weight1 and is worth value1, and the second item weighs weight2 and is worth value2. 
+# What is the total maximum value of the items you can take with you, assuming that your max weight capacity is maxW and you can't come back for 
+# the items later? Note that there are only two items and you can't bring more than one item of each type, i.e. you can't take two first items or
+# two second items.
