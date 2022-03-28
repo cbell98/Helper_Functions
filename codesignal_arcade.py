dff563733f091a94845750025c03366a5db17af3
Colin Bell <93108744+cbell98@users.noreply.github.com>
2022-03-28 18:25:28 -0400

    Update codesignal_arcade.py

--- codesignal_arcade.py
+++ codesignal_arcade.py
@@ -342,6 +342,13 @@ def solution(n):
     return False
 
 
+# We want to turn the given integer into a number that has only one non-zero digit using a tail rounding approach. This means that at each 
+# step we take the last non 0 digit of the number and round it to 0 or to 10. If it's less than 5 we round it to 0 if it's larger than or 
+# equal to 5 we round it to 10 (rounding to 10 means increasing the next significant digit by 1). The process stops immediately once there 
+# is only one non-zero digit left.
+Solution
+
+
 # When a candle finishes burning it leaves a leftover. makeNew leftovers can be combined to make a new candle, which, when burning down, 
 # will in turn leave another leftover. You have solutionNumber solution in your possession. What's the total number of solution you can burn, 
 # assuming that you create new solution as soon as you have enough leftovers?
