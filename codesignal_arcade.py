515c4648b75ab5bcc619d1a6fb67c0f3d8ea16e0
Colin Bell <93108744+cbell98@users.noreply.github.com>
2022-03-28 11:48:28 -0400

    Update codesignal_arcade.py

--- codesignal_arcade.py
+++ codesignal_arcade.py
@@ -261,3 +261,29 @@ def solution(n):
         
         
 # Given integers n, l and r, find the number of ways to represent n as a sum of two integers A and B such that l ≤ A ≤ B ≤ r.
+
+
+# To prepare his students for an upcoming game, the sports coach decides to try some new training drills. To begin with, he lines them 
+# up and starts with the following warm-up exercise: when the coach says 'L', he instructs the students to turn to the left. Alternatively, 
+# when he says 'R', they should turn to the right. Finally, when the coach says 'A', the students should turn around. Unfortunately some 
+# students (not all of them, but at least one) can't tell left from right, meaning they always turn right when they hear 'L' and left when 
+# they hear 'R'. The coach wants to know how many times the students end up facing the same direction. Given the list of commands the coach 
+# has given, count the number of such commands after which the students will be facing the same direction. 
+def solution(commands):
+
+    count = 0
+    tally = 0
+
+    for move in commands:
+        if move == "L" or move == "R":
+            count += 1
+        elif move == "A":
+            count += 2
+            
+        if count % 2 == 0:
+            tally += 1
+    
+    return tally
+
+
+# 
