a889fbb7bed30d7a00e414cfc74d0b6862be8431
Colin Bell <93108744+cbell98@users.noreply.github.com>
2022-03-31 18:51:42 -0400

    Update codesignal_arcade.py

--- codesignal_arcade.py
+++ codesignal_arcade.py
@@ -544,3 +544,11 @@ def solution(n):
 # an non-negative integer size. Since he likes to make things perfect, he wants to arrange them from smallest
 # to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional
 # statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
+def solution(statues):
+    statues.sort()
+    return ((statues[-1] - statues[0] + 1) - len(statues))
+
+
+# Given a sequence of integers as an array, determine whether it is possible to obtain a strictly increasing 
+# sequence by removing no more than one element from the array. Note: sequence a0, a1, ..., an is considered 
+# to be a strictly increasing if a0 < a1 < ... < an. Sequence containing only one element is also considered to be strictly increasing.
