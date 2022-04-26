15329f86be179536edb883c38453997136f52609
Colin Bell <93108744+cbell98@users.noreply.github.com>
2022-04-26 11:15:44 -0400

    Update codesignal_arcade.py

--- codesignal_arcade.py
+++ codesignal_arcade.py
@@ -739,4 +739,28 @@ def solution(startTag):
     return end_tag
 
 
+# A media access control address (MAC address) is a unique identifier assigned to network interfaces for communications on the 
+# physical network segment. The standard (IEEE 802) format for printing MAC-48 addresses in human-friendly form is six groups of 
+# two hexadecimal digits (0 to 9 or A to F), separated by hyphens (e.g. 01-23-45-67-89-AB). Your task is to check by given string 
+# inputString whether it corresponds to MAC-48 address or not.
+def solution(inputString):
+    
+    list = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
+    
+    if len(inputString) != 17:
+        return False
+        
+    for i in range(len(inputString)):
+
+        if i == 2 or i == 5 or i == 8 or i == 11 or i == 14:
+            if inputString[i] != "-":
+                return False
+        else:
+            if inputString[i] not in list:
+                # print(inputString[i])
+                return False
+
+    return True
+
+
 # 
