c69dbb7d9fb60e54a5bde6efdfae32ad17c546b0
Colin Bell <93108744+cbell98@users.noreply.github.com>
2022-04-26 13:28:01 -0400

    Update codesignal_arcade.py

--- codesignal_arcade.py
+++ codesignal_arcade.py
@@ -802,4 +802,8 @@ def solution(a, b):
     return min(counts)
 
 
-# 
+# A ciphertext alphabet is obtained from the plaintext alphabet by means of rearranging some characters. For example "bacdef...xyz" 
+# will be a simple ciphertext alphabet where a and b are rearranged. A substitution cipher is a method of encoding where each letter 
+# of the plaintext alphabet is replaced with the corresponding (i.e. having the same index) letter of some ciphertext alphabet.
+# Given two strings, check whether it is possible to obtain them from each other using some (possibly, different) substitution ciphers.
+
