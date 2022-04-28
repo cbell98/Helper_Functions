eb7febc8fa14e9a0ada1aa24b1d0f044b5e40663
Colin Bell <93108744+cbell98@users.noreply.github.com>
2022-04-28 13:44:14 -0400

    Update codesignal_arcade.py

--- codesignal_arcade.py
+++ codesignal_arcade.py
@@ -816,4 +816,7 @@ def solution(lst1, lst2):
     return res
 
 
-# 
+# There are some students standing in a row, each has some number written on their back. The students are about to divide into two
+# teams by counting off by twos: those standing at the even positions (0-based) will go to team A, and those standing at the odd 
+# position will join the team B. Your task is to calculate the difference between the sums of numbers written on the backs of the 
+# students that will join team A, and those written on the backs of the students that will join team B.
